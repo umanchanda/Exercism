8ce591c0614c3cd73505639ed78ddc1decd38428
Uday Manchanda <uday.manchanda14@gmail.com>
2020-03-11 02:00:12 -0400

    edit

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,12 +1,27 @@
 // Package bob does stuff
 package bob
 
-import "strings"
+import "unicode"
 
 // Hey is a function that
 func Hey(remark string) string {
-	if strings.Index(remark, "?") == (len(remark) - 1) {
+	if string(remark[len(remark)-1]) == "?" {
+		if IsUpper(remark) {
+			return "Calm down, I know what I'm doing!"
+		}
 		return "Sure."
+	} else if IsUpper(remark) {
+		return "Whoa, chill out!"
 	}
 	return "Whatever."
+}
+
+// IsUpper returns true if a given string is all uppercase
+func IsUpper(s string) bool {
+	for _, r := range s {
+		if !unicode.IsUpper(r) && unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
 }
