4564d9dd573c7f5c2898058e22d334e2d198fce4
Uday Manchanda <uday.manchanda14@gmail.com>
2020-03-14 01:10:57 -0400

    minor edits

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,7 +1,7 @@
-// Package twofer should have a
+// Package twofer contains a single method ShareWith
 package twofer
 
-// ShareWith should
+// ShareWith returns a message in the format of one for name, one for you
 func ShareWith(name string) string {
 	if name == "" {
 		return "One for you, one for me."
