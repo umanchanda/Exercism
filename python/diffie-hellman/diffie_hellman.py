1b0eb56c8de9e0af7555fecbfbd18bf350246c46
Uday Manchanda <uday.manchanda14@gmail.com>
2020-03-14 02:00:17 -0400

    finished diffie hellman

---
+++ python/diffie-hellman/diffie_hellman.py
@@ -0,0 +1,15 @@
+import secrets
+
+def private_key(p):
+    if secrets.randbelow(p) > p:
+        raise Exception("private key must be less than p")
+    else:
+        return secrets.randbelow(p)
+
+
+def public_key(p, g, private):
+    return (g**private) % p
+
+
+def secret(p, public, private):
+    return (public**private) % p
