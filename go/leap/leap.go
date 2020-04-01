37ecb1d8e8e2effa62109540a34277396541ab4c
Uday Manchanda <uday.manchanda14@gmail.com>
2020-04-01 06:13:47 +0000

    finished leap

---
+++ go/leap/leap.go
@@ -0,0 +1,14 @@
+// Package leap does 
+package leap
+
+// IsLeapYear should 
+func IsLeapYear(year int) bool {
+	if year % 400 == 0 {
+		return true
+	} else if year % 100 == 0 {
+		return false
+	} else if year % 4 == 0 {
+		return true
+	}
+	return false
+}
