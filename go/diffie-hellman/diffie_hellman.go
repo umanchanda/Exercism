b514e448327bf255ead8f3087ea9b544174e426c
Uday Manchanda <uday.manchanda14@gmail.com>
2021-12-19 02:19:09 -0500

    progress on diffie hellman

--- go/diffie-hellman/diffie_hellman.go
+++ go/diffie-hellman/diffie_hellman.go
@@ -3,25 +3,28 @@ package diffiehellman
 import (
 	"math/big"
 	"math/rand"
-	"math"
+	"time"
 )
 
 // PrivateKey generates a private key
 func PrivateKey(p *big.Int) *big.Int {
-	return rand.Intn(p)
+	src := rand.NewSource(time.Now().Unix())
+	r := rand.New(src)
+	return p.Rand(r, p)
 }
 
 // PublicKey generates public key
 func PublicKey(a, p *big.Int, g int64) *big.Int {
-	return math.Pow(g, a) % p
+
 }
 
 // SecretKey returns secret key
 func SecretKey(a, B, p *big.Int) *big.Int {
-	return math.Pow(B, a) % p
+	// return math.Pow(B, a) % p
+	return nil
 }
 
 // NewPair returns a new pair
 func NewPair(p *big.Int, g int64) {
-
+	return
 }
