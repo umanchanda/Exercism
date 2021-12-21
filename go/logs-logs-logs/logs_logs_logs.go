feda88cfe01ed119895babc2671b21303000e816
Uday Manchanda <uday.manchanda14@gmail.com>
2021-12-21 08:26:23 -0500

    finished logs logs logs

---
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -0,0 +1,35 @@
+package logs
+
+import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+)
+
+// Application identifies the application emitting the given log.
+func Application(log string) string {
+	if strings.Contains(log, "❗") {
+		return "recommendation"
+	} else if strings.Contains(log, "🔍") {
+		return "search"
+	} else if strings.Contains(log, "☀") {
+		return "weather"
+	} else {
+		return "default"
+	}
+	// panic("Please implement the Application() function")
+}
+
+// Replace replaces all occurrences of old with new, returning the modified log
+// to the caller.
+func Replace(log string, oldRune, newRune rune) string {
+	return strings.ReplaceAll(log, fmt.Sprintf("%c", oldRune), fmt.Sprintf("%c", newRune))
+	// panic("Please implement the Replace() function")
+}
+
+// WithinLimit determines whether or not the number of characters in log is
+// within the limit.
+func WithinLimit(log string, limit int) bool {
+	return utf8.RuneCountInString(log) <= limit
+	// panic("Please implement the WithinLimit() function")
+}
